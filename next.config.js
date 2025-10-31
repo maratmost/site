4d64f8102b59e34de166f640c0c71bdd2b459f6d
Maratmost <maratmost89@gmail.com>
2025-11-01 00:37:06 +0400

    Deploy site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  output: 'export', // статический экспорт для GitHub Pages
   images: {
+    unoptimized: true, // обязательно для статического экспорта
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [],
   },
+  basePath: isProd ? '/site' : '', // путь к репозиторию на GitHub Pages
+  assetPrefix: isProd ? '/site/' : '',
 }
 
 module.exports = nextConfig
